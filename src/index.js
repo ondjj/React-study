8ff4c8533df8106666bb7ccc47884abba7aceb4d
ondjj <jcjk0302@gmail.com>
2023-05-25 20:55:55 +0900

    State and Lifecycle ( Class Component 와 관련된 내용이지만 개념을 확실히 이해하는게 좋다.)
    State
    
    State는 리액트의 핵심중에 핵심
    리액트의 Component 상태를 의미 -> 리액트 Component의 변경 가능한 데이터
    state는 개발자가 정의한다.
    
    렌더링이나 데이터 흐름에 사용되는 값만 state에 포함시켜야한다.
    —> state가 변경될 경우 컴포넌트가 재 렌더링되기 때문에 성능이 저하될 수 있다.
    —>  렌더링이나 데이터 흐름에 관련 있는 값만 state, 그 외의 값은 컨포넌트의 인스턴스 필드로 정의한다.
    
    state는 자바스크립트 객체이다.
    
    한 번 정의한 state는 직접 수정할 수 없다. (엄밀이 말하면 하면 안된다.)
    -> setState 함수를 통한 수정이 정상적인 사용 방법이다.
    
    Lifecycle
    
    리액트 Component의가 계속 존재하는 것이 아니라, 시간의 흐름에 따라 생성되고 업데이트 되다가 사라진다.

--- src/index.js
+++ src/index.js
@@ -7,15 +7,17 @@ import reportWebVitals from './reportWebVitals';
 import Library from './chapter_03/Libray';
 import Clock from './chapter_04/Clock';
 import CommentList from './chapter_05/CommentList';
+import NotificationList from './chapter_06/NotificationList';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<NotificationList />);
 
-root.render(
-  <React.StrictMode>
-    <CommentList/>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// root.render(
+//   <React.StrictMode>
+//     <CommentList/>
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // setInterval(() => {
 // root.render(
