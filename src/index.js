2c6e7e459c6f34984cf221d2e86da4a1fa18fad7
ondjj <jcjk0302@gmail.com>
2023-05-26 11:11:10 +0900

    Event의 정의 및 Event 다루기
    이벤트는 사건이라는 의미를 가지고 있다. 특정 사건을 의미 ( 사용자가 버튼을 클릭하는 사건 -> 버튼 클릭 이벤트 )
    이 때 이벤트를 처리하는 것을 이벤트를 핸들링 한다고 표현한다.
    
    DOM에서 클릭을 통한 이벤트 처리는 onclick을 통해 자바스크립트 함수를 호출하는것으로 이루어진다.(문자열 그대로 전달함)
    onclick=“activate()”
    
    리액트에서 이벤트 처리는 onClick으로 카멜 케이스가 적용되어있고 중괄호로 함수이름을 감싸 전달한다.
    onClick={activate}
    
    이벤트 핸들러
    어떤 사건이 발생하면 사건을 처리하는 역할을 한다.
    Event Listener라고 부르기도한다.
    
    클래스 컴포넌트 :
    callback 에서 this를 사용하기 위해서는 바인딩을 필수적으로 해줘야 한다. -> 자바스크립트에서는 기본적으로 바인드가 이루어지지 않기 때문에
    글로벌 스코프로 지정되어 사용할 수 없기 때문이다.
    
    함수 컴포넌트:
    함수 컴포넌트에서는 클래스 컴포넌트 처럼 this를 사용하지않고 onClick에 곧 바로 이벤트 헨들러를 넣는것으로 대체할 수 있다.

--- src/index.js
+++ src/index.js
@@ -9,16 +9,28 @@ import Clock from './chapter_04/Clock';
 import CommentList from './chapter_05/CommentList';
 import NotificationList from './chapter_06/NotificationList';
 import Accommodate from './chapter_07/Accommodate';
+import ConfirmButton from './chapter_08/ComfirmButton';
+import FunctionButton from './chapter_08/FunctionButton';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// chapter 07
+// chapter 08
 root.render(
   <React.StrictMode>
-    <Accommodate/>
+    <ConfirmButton/>
+    <FunctionButton/>
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
+
+
+// chapter 07
+// root.render(
+//   <React.StrictMode>
+//     <Accommodate/>
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // chapter 06
 // root.render(<NotificationList />);
